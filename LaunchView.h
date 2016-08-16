d34ea8d0bba4291c52578075b873f381b22e906d
Mykola Tarasov <mykolatarasov@mykolas-macbook-pro.local>
2016-08-15 22:00:34 -0500

    new changes

---
+++ LaunchView.h
@@ -0,0 +1,15 @@
+//
+//  LaunchView.h
+//  dnd_mailer
+//
+//  Created by Mykola Tarasov on 8/15/16.
+//  Copyright © 2016 Mykola Tarasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchView : UITableViewController
+
+@property (nonatomic, weak) IBOutlet UIButton*signInButton;
+
+@end
---
+++ LaunchView.m
@@ -0,0 +1,31 @@
+//
+//  LaunchView.m
+//  dnd_mailer
+//
+//  Created by Mykola Tarasov on 8/15/16.
+//  Copyright © 2016 Mykola Tarasov. All rights reserved.
+//
+
+#import "LaunchView.h"
+
+@interface LaunchView ()
+
+@end
+
+@implementation LaunchView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    /*UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"launch_screen_image.jpg"]];
+    [self.view addSubview:imageView];*/
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
